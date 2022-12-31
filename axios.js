484fa204f6f0edd9e57f98edfd6514add24fa00c
frankwang <frankwang95174@gmail.com>
2022-12-31 12:16:44 +0800

    fix error of redirect

--- axios.js
+++ axios.js
@@ -1,8 +1,12 @@
 // get 'hexToken' from cookie if it existed
 const cookie = document.cookie;
-const cookieArray = cookie.split(';');
-const hexToken = cookieArray.find(item => item.includes('hexToken'));
-const token = hexToken.split('=')[1];
+console.log(cookie)
+let token = ''
+if(!!cookie) {
+    const cookieArray = cookie.split(';');
+    const hexToken = cookieArray.find(item => item.includes('hexToken'));
+    token = hexToken.split('=')[1];
+}
 
 export const instance = axios.create({
     baseURL: 'https://vue3-course-api.hexschool.io/v2',
